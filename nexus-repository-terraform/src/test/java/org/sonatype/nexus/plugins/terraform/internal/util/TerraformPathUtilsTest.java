e18bf0d0a258bb39b63cf93869e55c80067a0769
davejab <daveabrante@live.co.uk>
2022-02-24 16:40:10 +0000

    add path util test

--- nexus-repository-terraform/src/test/java/org/sonatype/nexus/plugins/terraform/internal/util/TerraformPathUtilsTest.java
+++ nexus-repository-terraform/src/test/java/org/sonatype/nexus/plugins/terraform/internal/util/TerraformPathUtilsTest.java
@@ -37,24 +37,113 @@ public class TerraformPathUtilsTest
 
   private Map<String, String> tokens;
 
+  private String discovery, api, providerPath, modulesPath, provider,
+          module, namespace, name, hostname, type, version, os, arch,
+          providerVersionsPath, providerVersionPath,
+          providerArchivePath, providerVersionDownloadPath;
+
   @Before
   public void setUp() {
     underTest = new TerraformPathUtils();
-    tokens = setupTokens("myTokenValue");
+
+    discovery = ".well-known/terraform.json";
+    api = "v1";
+    provider = "provider";
+    module = "module";
+    providerPath = String.format("%s/%ss", api, provider);
+    modulesPath = String.format("%s/%ss", api, module);
+    namespace = "namespace";
+    name = "name";
+    hostname = "hostname";
+    type = "type";
+    version = "version";
+    os = "os";
+    arch = "arch";
+
+    tokens = setupTokens();
     when(state.getTokens()).thenReturn(tokens);
+
+    providerVersionsPath = String.format("%s/%s/%s/%s/index.json", providerPath, hostname, namespace, type);
+    providerVersionPath = String.format("%s/%s/%s/%s/%s.json", providerPath, hostname, namespace, type, version);
+    providerArchivePath = String.format("%s/%s/%s/%s/%s-%s_%s_%s_%s.zip",
+            providerPath, hostname, namespace, type, provider, type, version, os, arch);
+    providerVersionDownloadPath = String.format("%s/%s/%s/%s/download/%s/%s",
+            providerPath, namespace, type, version, os, arch);
   }
 
-  // @Test
-  // public void buildAssetPath() {
-  //   String result = underTest.buildAssetPath(state, TerraformPathUtils.PACKAGE_FILENAME);
+  @Test
+  public void discoveryPath() {
+    String result = underTest.discoveryPath(state);
+    assertThat(result, is(equalTo(discovery)));
+  }
+  @Test
+  public void modulesPath() {
+    String expect = String.format("%s/index.json", modulesPath);
+    String result = underTest.modulesPath(state);
+    assertThat(result, is(equalTo(expect)));
+  }
 
-  //   assertThat(result, is(equalTo("/myTokenValue/myPackageFilename.txt")));
-  // }
+  @Test
+  public void moduleVersionsPath() {
+    String expect = String.format("%s/%s/%s/%s/index.json", modulesPath, namespace, name, provider);
+    String result = underTest.moduleVersionsPath(state);
+    assertThat(result, is(equalTo(expect)));
+  }
 
-  private Map<String, String> setupTokens(final String tokenValue) {
+  @Test
+  public void providersPath() {
+    String expect = String.format("%s/index.json", providerPath);
+    String result = underTest.providersPath(state);
+    assertThat(result, is(equalTo(expect)));
+  }
+
+  @Test
+  public void providerVersionsPath() {
+    String result = underTest.providerVersionsPath(state);
+    assertThat(result, is(equalTo(providerVersionsPath)));
+  }
+
+  @Test
+  public void providerVersionPath() {
+    String result = underTest.providerVersionPath(state);
+    assertThat(result, is(equalTo(providerVersionPath)));
+  }
+
+  @Test
+  public void providerArchivePath() {
+    String result = underTest.providerArchivePath(state);
+    assertThat(result, is(equalTo(providerArchivePath)));
+  }
+
+  @Test
+  public void toProviderVersionsPath() {
+    String expect = String.format("%s/%s/%s", providerPath, namespace, type);
+    String result = underTest.toProviderVersionsPath(providerVersionsPath, state);
+    assertThat(result, is(equalTo(expect)));
+  }
+
+  @Test
+  public void toProviderVersionDownloadPath() {
+    String result = underTest.toProviderVersionDownloadPath(providerVersionPath, os, arch, state);
+    assertThat(result, is(equalTo(providerVersionDownloadPath)));
+  }
+
+  @Test
+  public void toProviderArchiveDownloadPath() {
+    String result = underTest.toProviderArchiveDownloadPath(providerArchivePath, state);
+    assertThat(result, is(equalTo(providerVersionDownloadPath)));
+  }
+
+  private Map<String, String> setupTokens() {
     Map<String, String> tokens = new HashMap<>();
-    tokens.put("myTokenName", tokenValue);
-
+    tokens.put(namespace, namespace);
+    tokens.put(name, name);
+    tokens.put(provider, provider);
+    tokens.put(hostname, hostname);
+    tokens.put(type, type);
+    tokens.put(version, version);
+    tokens.put(os, os);
+    tokens.put(arch, arch);
     return tokens;
   }
 }
