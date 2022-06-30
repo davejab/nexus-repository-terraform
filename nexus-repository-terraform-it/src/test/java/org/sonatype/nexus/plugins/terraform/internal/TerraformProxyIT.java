0b3b5dbc1951dae992aeed82799e1d2a057ab497
davejab <13712138+davejab@users.noreply.github.com>
2022-06-30 21:23:46 +0100

    Integration tests fix (#16)
    * add missing dependency for it container
    
    * set up integration tests
    
    Co-authored-by: davejab <{ID}+{username}@users.noreply.github.com>

--- nexus-repository-terraform-it/src/test/java/org/sonatype/nexus/plugins/terraform/internal/TerraformProxyIT.java
+++ nexus-repository-terraform-it/src/test/java/org/sonatype/nexus/plugins/terraform/internal/TerraformProxyIT.java
@@ -12,34 +12,72 @@
  */
 package org.sonatype.nexus.plugins.terraform.internal;
 
+import org.junit.After;
+import org.junit.Before;
+import org.sonatype.goodies.httpfixture.server.fluent.Behaviours;
 import org.sonatype.goodies.httpfixture.server.fluent.Server;
 import org.sonatype.nexus.pax.exam.NexusPaxExamSupport;
 import org.sonatype.nexus.repository.Repository;
 import org.sonatype.nexus.repository.http.HttpStatus;
-import org.sonatype.nexus.testsuite.testsupport.FormatClientSupport;
+import org.sonatype.nexus.repository.storage.Asset;
 import org.sonatype.nexus.testsuite.testsupport.NexusITSupport;
 
-import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.sonatype.goodies.httpfixture.server.fluent.Behaviours.content;
-import static org.sonatype.goodies.httpfixture.server.fluent.Behaviours.error;
-// import static org.sonatype.nexus.plugins.terraform.internal.util.TerraformPathUtils.PACKAGE_FILENAME;
+import static org.hamcrest.Matchers.equalTo;
+import static org.sonatype.nexus.plugins.terraform.internal.util.TerraformPathUtils.PROVIDERS_PATH;
 import static org.sonatype.nexus.testsuite.testsupport.FormatClientSupport.status;
 
 public class TerraformProxyIT
     extends TerraformITSupport
 {
-  // @todo Change test path for your format
-  private static final String TEST_PATH = "some/valid/path/for/your/remote/"; //+ PACKAGE_FILENAME;
+  private static final String FORMAT_NAME = "terraform";
+
+  private static final String EXTENSION_JSON = ".json";
+
+  private static final String EXTENSION_ZIP = ".zip";
+
+  private static final String MIME_TYPE_JSON = "application/json";
+
+  private static final String MIME_TYPE_ZIP = "application/zip";
+
+  private static final String BAD_PATH = "this/path/is/bad";
+
+  private static final String HOSTNAME = "registry.terraform.io";
+
+  private static final String NAMESPACE = "hashicorp";
+
+  private static final String TYPE = "terraform";
+
+  private static final String VERSION = "0.1.0";
+
+  private static final String OS = "linux";
+
+  private static final String ARCH = "amd64";
+
+  private static final String PROVIDER_PATH = PROVIDERS_PATH + "/" + HOSTNAME + "/" + NAMESPACE + "/" + TYPE;
+
+  private static final String PROVIDER_INDEX = PROVIDER_PATH + "/index" + EXTENSION_JSON;
+
+  private static final String PROVIDER_VERSION = PROVIDER_PATH + "/" + VERSION + EXTENSION_JSON;
+
+  private static final String PROVIDER_ZIP = PROVIDER_PATH +
+          "/terraform-provider-" + TYPE + "_" + VERSION + "_" + OS + "_" + ARCH + EXTENSION_ZIP;
+
+  private static final String REMOTE_PROVIDER_PATH = PROVIDERS_PATH + "/" + NAMESPACE + "/" + TYPE;
+
+  private static final String REMOTE_DOWNLOAD_PATH = REMOTE_PROVIDER_PATH + "/" + VERSION +
+          "/download/" + OS + "/" + ARCH;
 
   private TerraformClient proxyClient;
 
   private Repository proxyRepo;
+
+  private Server server;
 
   @Configuration
   public static Option[] configureNexus() {
@@ -49,34 +87,68 @@ public class TerraformProxyIT
     );
   }
 
-  @Test
-  public void unresponsiveRemoteProduces404() throws Exception {
-    Server server = Server.withPort(0).serve("/*")
-        .withBehaviours(error(HttpStatus.NOT_FOUND))
-        .start();
-    try {
-      proxyRepo = repos.createTerraformProxy("terraform-test-proxy-notfound", server.getUrl().toExternalForm());
-      proxyClient = terraformClient(proxyRepo);
-      MatcherAssert.assertThat(FormatClientSupport.status(proxyClient.get(TEST_PATH)), is(HttpStatus.NOT_FOUND));
-    }
-    finally {
-      server.stop();
-    }
+  @Before
+  public void setup() throws Exception {
+    server = Server.withPort(0)
+            .serve("/" + REMOTE_PROVIDER_PATH)
+            .withBehaviours(Behaviours.file(testData.resolveFile(TYPE)))
+            .serve("/" + REMOTE_DOWNLOAD_PATH)
+            .withBehaviours(Behaviours.file(testData.resolveFile(ARCH)))
+            .start();
+    proxyRepo = repos.createTerraformProxy("terraform-test-proxy", server.getUrl().toExternalForm());
+    proxyClient = terraformClient(proxyRepo);
   }
 
   @Test
-  public void retrieveTerraformWhenRemoteOffline() throws Exception {
-    Server server = Server.withPort(0).serve("/*")
-        .withBehaviours(content("Response"))
-        .start();
-    try {
-      proxyRepo = repos.createTerraformProxy("terraform-test-proxy-offline", server.getUrl().toExternalForm());
-      proxyClient = terraformClient(proxyRepo);
-      proxyClient.get(TEST_PATH);
-    }
-    finally {
-      server.stop();
-    }
-    assertThat(status(proxyClient.get(TEST_PATH)), is(200));
+  public void unresponsiveRemoteProduces404() throws Exception {
+    assertThat(status(proxyClient.get(BAD_PATH)), is(HttpStatus.NOT_FOUND));
+  }
+
+  @Test
+  public void retrieveIndexJSONFromProxyWhenRemoteOnline() throws Exception {
+    assertThat(status(proxyClient.get(PROVIDER_INDEX)), is(HttpStatus.OK));
+
+    final Asset asset = findAsset(proxyRepo, PROVIDER_INDEX);
+    assertThat(asset.name(), is(equalTo(PROVIDER_INDEX)));
+    assertThat(asset.contentType(), is(equalTo(MIME_TYPE_JSON)));
+    assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
+    assertThat(asset.formatAttributes().get("hostname"), is(equalTo(HOSTNAME)));
+    assertThat(asset.formatAttributes().get("namespace"), is(equalTo(NAMESPACE)));
+    assertThat(asset.formatAttributes().get("type"), is(equalTo(TYPE)));
+  }
+
+  @Test
+  public void retrieveVersionJSONFromProxyWhenRemoteOnline() throws Exception {
+    assertThat(status(proxyClient.get(PROVIDER_VERSION)), is(HttpStatus.OK));
+
+    final Asset asset = findAsset(proxyRepo, PROVIDER_VERSION);
+    assertThat(asset.name(), is(equalTo(PROVIDER_VERSION)));
+    assertThat(asset.contentType(), is(equalTo(MIME_TYPE_JSON)));
+    assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
+    assertThat(asset.formatAttributes().get("hostname"), is(equalTo(HOSTNAME)));
+    assertThat(asset.formatAttributes().get("namespace"), is(equalTo(NAMESPACE)));
+    assertThat(asset.formatAttributes().get("type"), is(equalTo(TYPE)));
+    assertThat(asset.formatAttributes().get("version"), is(equalTo(VERSION)));
+  }
+
+  @Test
+  public void retrieveProviderZipFromProxyWhenRemoteOnline() throws Exception {
+    assertThat(status(proxyClient.get(PROVIDER_ZIP)), is(HttpStatus.OK));
+
+    final Asset asset = findAsset(proxyRepo, PROVIDER_ZIP);
+    assertThat(asset.name(), is(equalTo(PROVIDER_ZIP)));
+    assertThat(asset.contentType(), is(equalTo(MIME_TYPE_ZIP)));
+    assertThat(asset.format(), is(equalTo(FORMAT_NAME)));
+    assertThat(asset.formatAttributes().get("hostname"), is(equalTo(HOSTNAME)));
+    assertThat(asset.formatAttributes().get("namespace"), is(equalTo(NAMESPACE)));
+    assertThat(asset.formatAttributes().get("type"), is(equalTo(TYPE)));
+    assertThat(asset.formatAttributes().get("version"), is(equalTo(VERSION)));
+    assertThat(asset.formatAttributes().get("os"), is(equalTo(OS)));
+    assertThat(asset.formatAttributes().get("arch"), is(equalTo(ARCH)));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    server.stop();
   }
 }
